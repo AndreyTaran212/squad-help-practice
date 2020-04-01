61fdbb876620aa3eb2799ce316b605723362c5b8
AndreyTaran212 <tarangad313@gmail.com>
2020-04-01 19:53:17 +0300

    first commit

---
+++ client/src/components/FormInputs/FormInputs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const FormInputs = (props) => {
+    const {label, input, type, classes, meta: {touched, error}} = props;
+    const inputClassName = classNames(classes.input, {
+        [classes.valid]: touched && !error,
+        [classes.notValid]: touched && error,
+    });
+    return (
+        <div className={classes.container}>
+            <input {...input} placeholder={label} type={type} className={inputClassName}
+            />
+            {classes.warning && (touched && (error && <span className={classes.warning}>{error}</span>))}
+        </div>
+    )
+};
+
+export default FormInputs;
